314cc1b5634c66f8fa9f72f40cb11c9fc182e2ea
Fred <frederico.a.soares@gmail.com>
2021-02-01 20:47:39 -0300

    Added return on useEffect. Playing with web-vitals. Added socket.emit function to send message.

--- react-socketio/src/App.js
+++ react-socketio/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import socketIOClient from "socket.io-client";
+
+import { getCLS, getFID, getLCP } from 'web-vitals';
+
 const ENDPOINT = "http://192.168.15.8:3500/";
 
 function App() {
@@ -11,14 +14,29 @@ function App() {
     const socket = socketIOClient(ENDPOINT);
     socket.on("chat message", data => {
       setResponse(data);
-      if( data === "register" ) {
+      if (data === "register") {
         history.push("/game");
       }
     });
-  }, []);
+
+    return () => {
+      socket.on("disconnect", () => {
+        console.log('user disconnected');
+      });
+    }
+
+  }, [] /* it will run once */);
 
   var register = () => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.emit("chat message", "xxxx");
     setResponse("register");
+
+
+
+    getCLS(console.log);
+    getFID(console.log);
+    getLCP(console.log);
   }
 
   return (
@@ -27,7 +45,7 @@ function App() {
         It's <time dateTime={response}>{response}</time>
       </p>
       <button onClick={register}>I want to play</button>
-      
+
     </div>
   );
 }
